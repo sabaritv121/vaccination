702c22addabfb8c8e18dfb8c9e2a0281f72943e8
sabaritv121 <97951510+sabaritv121@users.noreply.github.com>
2022-01-19 11:19:45 +0530

    commit

--- vaccination_management/urls.py
+++ vaccination_management/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vaccination_app.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
